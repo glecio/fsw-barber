a7c94520ac8800c175ab9ca3c6e6f60baa1502a1
Glecio Santos <glecio.santos@gmail.com>
2024-08-07 18:02:31 -0300

    feat: avatar component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
 import { Card, CardContent } from "./_components/ui/card"
@@ -41,10 +42,27 @@ const Home = () => {
         {/* AGENDAMENTO */}
 
         <Card className="mt-6">
-          <CardContent className="flex">
+          <CardContent className="flex justify-between">
             {/* ESQUERDA */}
             <div className="flex flex-col gap-2 py-5">
-              <Badge>Confirmado</Badge>
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3>Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage
+                    src="/logo-vintage-barber.png"
+                    alt="logo-vintage-barber"
+                  />
+                </Avatar>
+                <p className="text-sm">Vintage Barber</p>
+              </div>
+            </div>
+
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border border-slate-400">
+              <p>Agosto</p>
+              <p className="py-1 text-5xl">05</p>
+              <p className="text-xl">21:00</p>
             </div>
           </CardContent>
         </Card>
