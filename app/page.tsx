93eefbe9b127aed075b7dfd695827f0364fc86fe
Glecio Santos <glecio.santos@gmail.com>
2024-08-06 21:39:32 -0300

    feat: card atendimento

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import Image from "next/image"
 import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
-import { SearchIcon } from "lucide-react"
+import { Badge, SearchIcon } from "lucide-react"
+import { Card, CardContent } from "./_components/ui/card"
 
 const Home = () => {
   const [] = useState()
@@ -14,16 +15,19 @@ const Home = () => {
     <div>
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Glécio </h2>
+        <h2 className="text-xl font-bold">Olá, Glécio!</h2>
         <p>Segunda-feira, 05 de agosto de 2024</p>
-        <div className="mt-24 flex items-center gap-2">
-          <Input />
+
+        {/* BUSCA */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca" />
           <Button>
             <SearchIcon />
           </Button>
         </div>
 
-        <div className="relative h-[150px] w-full">
+        {/* BANNER 01 */}
+        <div className="relative mt-6 h-[150px] w-full rounded-xl">
           <Image
             alt="Agende nas melhores barbearias"
             src="/banner-01.png"
@@ -31,6 +35,18 @@ const Home = () => {
             fill
           />
         </div>
+
+        {/* AGENDAMENTO */}
+
+        <Card className="mt-6">
+          <CardContent className="flex">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5">
+              <Badge>Confirmado</Badge>
+              <h3>teste</h3>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
