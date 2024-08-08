21e38c4924641b3cba20e7f45b4c329b174886d4
Glecio Santos <glecio.santos@gmail.com>
2024-08-08 09:30:13 -0300

    feat: initial barbershop page

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { Button } from "./ui/button"
 import { Badge } from "./ui/badge"
 import { StarIcon } from "lucide-react"
+import Link from "next/link"
 
 interface BarberShopItemProps {
   barberShop: Barbershop
@@ -36,8 +37,8 @@ const BarberShopItem = ({ barberShop }: BarberShopItemProps) => {
             {barberShop.name}
           </h3>
           <p className="truncate text-sm text-gray-400">{barberShop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full">
-            Reservar
+          <Button variant="secondary" className="mt-3 w-full" asChild>
+            <Link href={`/barbershops/${barberShop.id}`}>Reservar</Link>
           </Button>
         </div>
       </CardContent>
