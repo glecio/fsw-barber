117e09a2490fd6fb4fc566e722613c49c526b989
Glecio Santos <glecio.santos@gmail.com>
2024-08-09 09:53:25 -0300

    feat: footer component and toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { Toaster } from "./_components/ui/toaster"
+import { Toaster } from "./_components/ui/sonner"
+import Footer from "./_components/footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="pt-BR" className="dark">
       <body className={inter.className}>
         <div className="p-4">{children}</div>
+        <Footer />
       </body>
       <Toaster />
     </html>
